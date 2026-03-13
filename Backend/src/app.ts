d6333bc487d3a11af2e1dadbe82854a615fe6ccb
Akshat Jain <gomat2230@gmail.com>
2026-03-13 19:48:08 +0530

    setup structure for backend

---
+++ Backend/src/app.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors"
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+
+app.use(cors({
+  origin: ["http://localhost:5173", "http://localhost:8000", "*"],
+  credentials: true
+}));
+
+app.get("/health", (_, res) => {
+    res.send("api is running");
+});
+
+export default app;
